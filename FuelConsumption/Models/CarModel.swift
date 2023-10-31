21fadb87f0bc90dae1e71dde3246fc9e296dc7fb
Maciej Rak <raczek.maciek@gmail.com>
2023-10-31 13:40:17 +0100

    create CarModel struct

--- FuelConsumption/Models/CarModel.swift
+++ FuelConsumption/Models/CarModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+enum fuelTypes: String{
+    case gasoline = "gasoline"
+    case diesel = "diesel"
+    case lpg = "LPG"
+    case cng = "CNG"
+    
+    static let allFuelTypes = [gasoline, diesel, lpg, cng]
+}
+
+struct RefuelingHistory{
+    var date: Date
+    var fuelAmount: Double
+    var moneyPaid: Double
+}
+
+struct CarModel: Identifiable{
+    let id: String
+    var carBrand: String
+    var carModel: String
+    var fuelType: String
+    var refuelingHistory: [RefuelingHistory]
+    var engineSize: Int // In cc for example 1975cc 1.975 l
+    var averageConsumption: Double // App will have three types of average consumption - mpg, kpl, l/100km
+}
