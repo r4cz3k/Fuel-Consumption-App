a0dcb8eb85f45ed6dbd13df09ff7147936527e35
Maciej Rak <raczek.maciek@gmail.com>
2023-10-31 13:20:56 +0100

    Create CarsView, CarsViewModel, CarModel

---
+++ FuelConsumption/Models/CarModel.swift
@@ -0,0 +1,8 @@
+//
+//  CarModel.swift
+//  FuelConsumption
+//
+//  Created by Maciej Rak on 31/10/2023.
+//
+
+import Foundation
---
+++ FuelConsumption/ViewModels/CarsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CarsViewModel.swift
+//  FuelConsumption
+//
+//  Created by Maciej Rak on 31/10/2023.
+//
+
+import Foundation
