6df8b4e456c2ef7e08793491612e5590a9056a35
Maciej Rak <raczek.maciek@gmail.com>
2023-10-31 13:18:08 +0100

    Create MVVM folder structure

---
+++ FuelConsumption/Views/CarsView.swift
@@ -0,0 +1,18 @@
+//
+//  CarsView.swift
+//  FuelConsumption
+//
+//  Created by Maciej Rak on 31/10/2023.
+//
+
+import SwiftUI
+
+struct CarsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarsView()
+}
