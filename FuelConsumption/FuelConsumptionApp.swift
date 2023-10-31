027ffb9790f902c27cc946502930671baf0e8650
Maciej Rak <raczek.maciek@gmail.com>
2023-10-31 13:13:23 +0100

    Initial Commit

---
+++ FuelConsumption/FuelConsumptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelConsumptionApp.swift
+//  FuelConsumption
+//
+//  Created by Maciej Rak on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FuelConsumptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
