6809985c7576d5de4bd05806933cb9ac4167ece2
Maciej Rak <raczek.maciek@gmail.com>
2023-11-07 23:05:42 +0100

    Update CarDetailsView.swift

--- FuelConsumption/Views/CarDetailsView.swift
+++ FuelConsumption/Views/CarDetailsView.swift
@@ -12,19 +12,53 @@ struct CarDetailsView: View {
     var car: CarModel
     
     var body: some View {
-        NavigationStack{
-            
-        }
-        .toolbar{
-            ToolbarItem(placement: .topBarTrailing) {
-                NavigationLink(
-                    destination: Text("Add Refueling"),
-                    label: {
-                        Text("Add")
+        VStack{
+            VStack(alignment: .leading, spacing: 20){
+                HStack{
+                    Text(car.carBrand)
+                        .bold()
+                    Text(car.carModel)
+                    Spacer()
+                    Text("\(String(car.averageConsumption)) L / 100 km")
+                        .font(.title2)
+                }
+                .font(.largeTitle)
+                
+                CarInfoRowView(
+                    infoTitle: "Registration Number",
+                    infoData: car.registrationNumber)
+                
+                CarInfoRowView(
+                    infoTitle: "Year Of Production",
+                    infoData: String(car.yearOfProduction))
+                
+                CarInfoRowView(
+                    infoTitle: "Fuel Type",
+                    infoData: car.fuelType.capitalized)
+                
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Last Refueling:")
+                        .font(.title)
+                        .bold()
+                    
+                    VStack(alignment: .leading, spacing: 10){
+                        CarInfoRowView(
+                            infoTitle: "Date",
+                            infoData: String(car.refuelingHistory[car.refuelingHistory.count - 1].date.formatted(date: .numeric, time: .omitted)))
+                        
+                        CarInfoRowView(
+                            infoTitle: "Fuel Amount",
+                            infoData: "\(String(car.refuelingHistory[car.refuelingHistory.count - 1].fuelAmount)) L")
+                        
+                        CarInfoRowView(
+                            infoTitle: "Money Amount",
+                            infoData: "\(String(car.refuelingHistory[car.refuelingHistory.count - 1].moneyPaid)) PLN")
                     }
-                )
+                    .padding(.horizontal)
+                }
             }
         }
+        .padding()
     }
 }
 
